0b30d3db5518c0ca3ba427bdd74312e54e6f5af9
HarrisonGrubb <harrison.grubb@stern.nyu.edu>
2019-06-23 16:09:28 -0400

    throw away

---
+++ exam.py
@@ -0,0 +1,16 @@
+delivery = {
+'sender': 'Charlie',
+'recipient': 'Anika',
+'price': 16.99,
+'status': 'In Transit',
+'stops': ['New York', 'Denver', 'San Francisco']
+}
+
+print('A delivery from ', delivery['sender'], ' to ', delivery['recipient'])
+
+stops = 0
+for s in delivery['stops']:
+    stops += 1
+print(stops)
+
+print(delivery['stops'][-1:])
